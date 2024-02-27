e20657a29993f57405efdf5d629fe562d1b59a8d
renaldo-santos-franca <renaldo.santos.franca@ccc.ufcg.edu.br>
2024-02-27 18:01:22 -0300

    Primeiro commit

---
+++ ordenacao/InsertionAndSelection/Insere_Primeiro.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Insere_Primeiro{
+
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		String[] array = sc.nextLine().split(" ");
+		int[] nums = new int[array.length];
+
+		for(int i=0; i<nums.length; i++){
+
+			nums[i] = Integer.parseInt(array[i]);
+
+		}
+
+		int aux, i = 0;
+		while(i < nums.length - 1 && nums[i] > nums[i+1]){
+
+			aux = nums[i];
+			nums[i] = nums[i+1];
+			nums[i+1] = aux;
+			i++;
+
+		} 
+
+		System.out.println(Arrays.toString(nums));
+
+	}
+
+}
